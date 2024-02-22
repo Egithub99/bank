e0f27c893d9a0b08b7444d8c0efaff75af7615f3
Ed <edward.de.groot@xs4all.nl>
2024-02-22 18:48:14 +0100

    changes les2

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
 const routes = {
-    '/login': { 
-      templateId: 'login', 
-      title: 'Login - Bank App',
-      onShow: () => {}
-    },
-    '/dashboard': { 
-      templateId: 'dashboard', 
-      title: 'Dashboard - Bank App',
-      onShow: () => { console.log('Dashboard is shown'); }
-    },
-    // Add other routes as needed
-  };
-  
-  function updateRoute() {
-    const path = window.location.pathname;
-    const route = routes[path];
-  
-    if (!route) {
-      return navigate('/login');
-    }
-  
-    const template = document.getElementById(route.templateId);
-    const view = template.content.cloneNode(true);
-    const app = document.getElementById('app');
-    app.innerHTML = '';
-    app.appendChild(view);
-  
-    // Update the window title
-    document.title = route.title;
-  
-    // Run the onShow function
-    route.onShow();
+  '/login': { 
+    templateId: 'login', 
+    title: 'Login - Bank App',
+    onShow: () => {}
+  },
+  '/dashboard': { 
+    templateId: 'dashboard', 
+    title: 'Dashboard - Bank App',
+    onShow: () => { console.log('Dashboard is shown'); }
+  },
+  // Add other routes as needed
+};
+
+function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
+
+  if (!route) {
+    return navigate('/login');
   }
-  
-  function navigate(path) {
-    window.history.pushState({}, path, path);
-    updateRoute();
-  }
-  
-  window.onpopstate = () => updateRoute();
-  
-  // Update the route for the first time
+
+  const template = document.getElementById(route.templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById('app');
+  app.innerHTML = '';
+  app.appendChild(view);
+
+  // Update the window title
+  document.title = route.title;
+
+  // Run the onShow function
+  route.onShow();
+}
+
+function navigate(path) {
+  window.history.pushState({}, path, path);
   updateRoute();
-  
+}
 
+window.onpopstate = () => updateRoute();
 
- 
+// Update the route for the first time
+updateRoute();
+
+async function createAccount(account) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: account
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
+async function register() {
+  const registerForm = document.getElementById('registerForm');
+  const formData = new FormData(registerForm);
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    return console.log('An error occurred:', result.error);
+  }
+
+  console.log('Account created!', result);
+}
